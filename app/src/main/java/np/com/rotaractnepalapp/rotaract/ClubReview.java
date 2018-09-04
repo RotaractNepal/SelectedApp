21d435262113fed9508be58cdb307d092bd8d018
RotaractNepalApp <35910030+rotaractnepalapp@users.noreply.github.com>
2018-09-04 14:23:40 +0545

    adding club and members layout

--- app/src/main/java/np/com/rotaractnepalapp/rotaract/ClubReview.java
+++ app/src/main/java/np/com/rotaractnepalapp/rotaract/ClubReview.java
@@ -1,15 +1,11 @@
 package np.com.rotaractnepalapp.rotaract;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 public class ClubReview extends AppCompatActivity {
 
@@ -28,7 +24,7 @@ public class ClubReview extends AppCompatActivity {
                 AlertDialog.Builder builder = new AlertDialog.Builder(ClubReview.this);
 
                 LayoutInflater inflater = getLayoutInflater();
-                View dialogView = inflater.inflate(R.layout.club_bod_info,null);
+                View dialogView = inflater.inflate(R.layout.club_mem_info,null);
 
                 builder.setView(dialogView);
 
