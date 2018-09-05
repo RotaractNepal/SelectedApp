d9c68f136488394222307fbdb1bf7058da471d34
RotaractNepalApp <35910030+rotaractnepalapp@users.noreply.github.com>
2018-09-05 09:58:41 +0545

    some edit of club mem info display layout

--- app/src/main/java/np/com/rotaractnepalapp/rotaract/ClubReview.java
+++ app/src/main/java/np/com/rotaractnepalapp/rotaract/ClubReview.java
@@ -8,10 +8,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ClubReview extends AppCompatActivity {
 
-    private ImageView ITofficer;
+    private ImageView ITofficer,President;
+    private ImageView clubBODProfile, bodClubLogo;
+    private TextView clubBODName, clubBODDes;
     private ImageView memCall, memEmail, memClose;
 
     @Override
@@ -20,8 +23,67 @@ public class ClubReview extends AppCompatActivity {
         setContentView(R.layout.activity_club_review);
         this.setTitle("Club Review");
 
+        President = (ImageView) findViewById(R.id.clubPresident);
         ITofficer = (ImageView) findViewById(R.id.clubITOfficer);
 
+        President.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ClubReview.this);
+
+                LayoutInflater inflater = getLayoutInflater();
+                View dialogView = inflater.inflate(R.layout.club_mem_info,null);
+
+                builder.setView(dialogView);
+
+                memCall = (ImageView) dialogView.findViewById(R.id.call);
+                memEmail = (ImageView) dialogView.findViewById(R.id.email);
+                memClose = (ImageView) dialogView.findViewById(R.id.cancel);
+
+                clubBODProfile = (ImageView) dialogView.findViewById(R.id.clubBODprofile);
+                clubBODProfile.setImageResource(R.drawable.rackne_president);
+
+                bodClubLogo = (ImageView) dialogView.findViewById(R.id.bodClubLogo);
+                bodClubLogo.setImageResource(R.drawable.ic_rckne);
+
+                clubBODName = (TextView) dialogView.findViewById(R.id.clubBODname);
+                clubBODName.setText(getString(R.string.preRACKNEName));
+
+                clubBODDes = (TextView) dialogView.findViewById(R.id.clubBODdes);
+                clubBODDes.setText(getString(R.string.preRACKNEDesignation));
+
+                final AlertDialog dialog = builder.create();
+
+                memCall.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        String contactNo = getString(R.string.preRACKNEContact);
+                        clubMemCall(v, contactNo);
+
+                    }
+                });
+
+                memEmail.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        String[] to = {getString(R.string.preRACKNEEmail)};
+                        clubMemEmail(v, to);
+
+                    }
+                });
+
+                memClose.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.cancel();
+                    }
+                });
+
+                dialog.show();
+            }
+        });
 
         ITofficer.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,32 +99,37 @@ public class ClubReview extends AppCompatActivity {
                 memEmail = (ImageView) dialogView.findViewById(R.id.email);
                 memClose = (ImageView) dialogView.findViewById(R.id.cancel);
 
+                clubBODProfile = (ImageView) dialogView.findViewById(R.id.clubBODprofile);
+                clubBODProfile.setImageResource(R.drawable.rackne_itofficer);
+
+                bodClubLogo = (ImageView) dialogView.findViewById(R.id.bodClubLogo);
+                bodClubLogo.setImageResource(R.drawable.ic_rckne);
+
+                clubBODName = (TextView) dialogView.findViewById(R.id.clubBODname);
+                clubBODName.setText(getString(R.string.itofficerRACKNEName));
+
+                clubBODDes = (TextView) dialogView.findViewById(R.id.clubBODdes);
+                clubBODDes.setText(getString(R.string.itofficerRACKNEDesignation));
+
                 final AlertDialog dialog = builder.create();
 
                 memCall.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        // Toast.makeText(MainActivity.this, "Call Pressed", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(Intent.ACTION_DIAL);
-                        String contactNo = getString(R.string.itoffficerContact);
-                        intent.setData(Uri.parse("tel:" + contactNo));
-                        startActivity(intent);
+
+                        String contactNo = getString(R.string.itofficerRACKNEContact);
+                        clubMemCall(v, contactNo);
+
                     }
                 });
 
                 memEmail.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        Intent intenEmail=null, chooserEmail=null;
-                        intenEmail = new Intent(Intent.ACTION_SEND);
-                        intenEmail.setData(Uri.parse("mailto:"));
-                        String[] to = {getString(R.string.itoffficerEmail)};
-                        intenEmail.putExtra(Intent.EXTRA_EMAIL, to);
-                        intenEmail.putExtra(Intent.EXTRA_SUBJECT, "Sent From Rotaract Nepal App");
-                        intenEmail.putExtra(Intent.EXTRA_TEXT, "Hellow Sir");
-                        intenEmail.setType("message/rfc822");
-                        chooserEmail = intenEmail.createChooser(intenEmail,"Send Email");
-                        startActivity(chooserEmail);
+
+                        String[] to = {getString(R.string.itofficerRACKNEEmail)};
+                        clubMemEmail(v, to);
+
                     }
                 });
 
@@ -77,6 +144,24 @@ public class ClubReview extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void clubMemCall(View v, String contactNo){
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + contactNo));
+        startActivity(intent);
+    }
+
+    public void clubMemEmail(View v, String[] to){
+        Intent intenEmail=null, chooserEmail=null;
+        intenEmail = new Intent(Intent.ACTION_SEND);
+        intenEmail.setData(Uri.parse("mailto:"));
+        intenEmail.putExtra(Intent.EXTRA_EMAIL, to);
+        intenEmail.putExtra(Intent.EXTRA_SUBJECT, "Sent From Rotaract Nepal App");
+        intenEmail.putExtra(Intent.EXTRA_TEXT, "Hellow Sir");
+        intenEmail.setType("message/rfc822");
+        chooserEmail = intenEmail.createChooser(intenEmail,"Send Email");
+        startActivity(chooserEmail);
     }
 
 }
