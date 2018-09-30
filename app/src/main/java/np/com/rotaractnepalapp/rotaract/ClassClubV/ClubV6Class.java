b6d28b95e8ee6018a1379679e014faacb4c35a48
RotaractNepalApp <35910030+rotaractnepalapp@users.noreply.github.com>
2018-09-30 11:05:09 +0545

    added class for zone club

---
+++ app/src/main/java/np/com/rotaractnepalapp/rotaract/ClassClubV/ClubV6Class.java
@@ -0,0 +1,80 @@
+package np.com.rotaractnepalapp.rotaract.ClassClubV;
+
+public class ClubV6Class {
+    private int images;
+    private String title;
+    private int bimages;
+    private String name;
+    private String ridno;
+    private String call;
+    private String email;
+
+    public ClubV6Class() {
+    }
+
+    public ClubV6Class(int images, String title, int bimages, String name, String ridno, String call, String email) {
+        this.images = images;
+        this.title = title;
+        this.bimages = bimages;
+        this.name = name;
+        this.ridno = ridno;
+        this.call = call;
+        this.email = email;
+    }
+
+    public int getImages() {
+        return images;
+    }
+
+    public void setImages(int images) {
+        this.images = images;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getBimages() {
+        return bimages;
+    }
+
+    public void setBimages(int bimages) {
+        this.bimages = bimages;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRidno() {
+        return ridno;
+    }
+
+    public void setRidno(String ridno) {
+        this.ridno = ridno;
+    }
+
+    public String getCall() {
+        return call;
+    }
+
+    public void setCall(String call) {
+        this.call = call;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
