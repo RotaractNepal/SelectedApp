df0bc4c1fa4c87347a2f82d77a18a843bd8b76ca
RotaractNepalApp <35910030+rotaractnepalapp@users.noreply.github.com>
2018-10-02 15:11:13 +0545

    modified main page intro

--- app/src/main/java/np/com/rotaractnepalapp/rotaract/Adapter/IntroductionAdapter/Intro1Adpater.java
+++ app/src/main/java/np/com/rotaractnepalapp/rotaract/Adapter/IntroductionAdapter/Intro1Adpater.java
@@ -1,10 +1,16 @@
 package np.com.rotaractnepalapp.rotaract.Adapter.IntroductionAdapter;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -46,6 +52,32 @@ public class Intro1Adpater extends RecyclerView.Adapter<Intro1Adpater.ClubViewHo
             super(itemView);
             title = (TextView) itemView.findViewById(R.id.nameTextView);
             images = (CircleImageView) itemView.findViewById(R.id.imageView);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION){
+                        final Intro1Class clickedDataItem = data.get(position);
+                        final Dialog Information;
+                        Information = new Dialog(v.getContext());
+
+                        Information.setContentView(R.layout.intro_item_adapter_layout);
+
+                        TextView close = (TextView) Information.findViewById(R.id.closeDialog);
+                        close.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Information.dismiss();
+                            }
+                        });
+
+                        Information.setCanceledOnTouchOutside(false);
+                        Information.setCancelable(false);
+                        Information.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                        Information.show();
+                    }
+                }
+            });
         }
     }
 }
